95939989fa99dc12395406575dc531d5c04d5c6e
tricialyjun <37462972+tricialyjun@users.noreply.github.com>
2022-12-01 23:31:57 +0800

    Update annotation in day01.py

--- Day01/day01.py
+++ Day01/day01.py
@@ -4,6 +4,7 @@ Created on Thu Dec  1 22:40:08 2022
 @author: tricialyjun
 """
 
+# Part 1
 with open('calories', 'r') as f:
     inventories = f.read().split("\n\n")
 
@@ -14,5 +15,6 @@ for ii in range(len(inventories)):
 
 print(max(total_calories))
 
+# Part 2
 total_calories.sort(reverse=True)
 print(sum(total_calories[0:3]))
